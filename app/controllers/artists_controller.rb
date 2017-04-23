37fd235dd59f57ea1ebf3864853f260490a8c5e2
Fmagro <fmagro@hotmail.it>
2017-04-23 12:37:16 +0200

    creazione azione managesong su artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,30 +5,9 @@ def index
  
   def show
     @artist = Artist.find(params[:id])
+
   end
- 
-  def new
-    @artist = Artist.new
-  end
- 
-  def edit
-    @artist = Artist.find(params[:id])
-  end
- 
-  def create
-    @artist = Artist.new(artist_params)
-    if params[:song_add]
-      @song = Song.where(:id => params[:song_to_add])
-      @artist.songs<< @song
-    end
-    if @artist.save
-      redirect_to @artist
-    else
-      render 'new'
-    end
-  end
- 
-  def update
+   def managesong
     @artist = Artist.find(params[:id])
     if params[:song_add]
       @song = Song.where(:id  => params[:song_to_add])
@@ -38,6 +17,31 @@ def index
       @songd = Song.where(:id  => params[:song_to_delete])
       @artist.songs.delete(@songd)
     end
+    redirect_to @artist
+  end
+ 
+  def new
+    @artist = Artist.new
+    @artist.isgroup = false
+  end
+ 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+ 
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to @artist
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @artist = Artist.find(params[:id])
+
     if @artist.update(artist_params)
       redirect_to @artist
     else
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
   has_and_belongs_to_many :songs 
   accepts_nested_attributes_for :songs
-  validates :name, presence: true,
+  validates :name, presence: true, uniqueness: true,
                     length: { minimum: 1, maximum: 25 }
 
 
