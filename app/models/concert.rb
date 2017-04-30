0f424c47543aa7d87ad4ebd371b7e99efe0d095c
Fmagro <fmagro@hotmail.it>
2017-04-30 16:11:00 +0200

    association between artist and concert

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,7 +1,11 @@
 class Concert < ApplicationRecord
+  has_and_belongs_to_many :artists
+  accepts_nested_attributes_for :artists 
+
   has_many :setlists, dependent: :destroy
   has_many :songs, :through => :setlists
-  accepts_nested_attributes_for :setlists	
+  accepts_nested_attributes_for :setlists
+	
   validates :name, presence: true,
                     length: { minimum: 1, maximum: 25 }
   validates :date, presence: true
