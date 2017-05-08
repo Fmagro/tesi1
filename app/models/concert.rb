2d02396fc0d2a4d12f2d07761bfe460a7ab9f1af
Fmagro <fmagro@hotmail.it>
2017-05-08 18:44:05 +0200

    added inverse_of on all associations except band

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,7 +2,7 @@ class Concert < ApplicationRecord
   has_and_belongs_to_many :artists
   accepts_nested_attributes_for :artists 
 
-  has_many :setlists, dependent: :destroy
+  has_many :setlists, inverse_of: :concert,      dependent: :destroy
   has_many :songs, :through => :setlists
   accepts_nested_attributes_for :setlists
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-  has_many :setlists, dependent: :destroy
+  has_many :setlists, inverse_of: :song, dependent: :destroy
   has_many :concerts, :through => :setlists
   has_and_belongs_to_many :artists 
   def self.all_genres; %w[pop rock metal classical jazz alternative hiphop blues country lyrical]; end
