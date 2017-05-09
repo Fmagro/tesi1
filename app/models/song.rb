d3fc7fc7278d8e7f8a9a96b4f7e8e44c3a24d971
Fmagro <fmagro@hotmail.it>
2017-05-09 21:01:22 +0200

    added query for song

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,11 @@ class Song < ApplicationRecord
   has_and_belongs_to_many :artists 
   def self.all_genres; %w[pop rock metal classical jazz alternative hiphop blues country lyrical]; end
  
-  validates :title, presence: true, uniqueness: true,
+  validates :title, presence: true, 
              length: {maximum: 25 }
   validates :genre, inclusion: { in:  Song.all_genres }
+
+  scope :by_title, -> (title_s) { where("title = ?", title_s) if title_s.present? }
+  scope :by_genre, -> (genre_s) { where("genre = ?", genre_s) if genre_s.present? }
+  scope :by_artist, -> (artist_s) { joins(:artists).where("name= ?", artist_s) if artist_s.present? }
 end
